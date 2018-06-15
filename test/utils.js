a5e0e7376713e82ae5f189cf3761ef1d89f8bdf3
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2018-06-15 22:13:57 +0700

    Review fixes

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,5 @@
+import increaseTime from "../node_modules/zeppelin-solidity/test/helpers/increaseTime.js";
+
 export function sig(address, opts) {
   if (typeof opts === "undefined") {
     opts = {};
@@ -14,25 +16,7 @@ export function sig(address, opts) {
 
 export function timeTravelTo(date) {
   const duration = new Date(date).getTime() / 1000 - web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1;
-  const id = Date.now();
 
-  return new Promise((resolve, reject) => {
-    web3.currentProvider.sendAsync({
-      jsonrpc: '2.0',
-      method: 'evm_increaseTime',
-      params: [duration],
-      id: id,
-    }, err1 => {
-      if (err1) return reject(err1)
-
-      web3.currentProvider.sendAsync({
-        jsonrpc: '2.0',
-        method: 'evm_mine',
-        id: id+1,
-      }, (err2, res) => {
-        return err2 ? reject(err2) : resolve(res)
-      })
-    })
-  })
+  increaseTime(duration);
 }
 
