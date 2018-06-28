91ebd2b4b7da59ad3f5f8b42204e5b9b32990284
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2018-06-28 16:08:55 +0700

    Fix review comments

--- contracts/Burner.sol
+++ contracts/Burner.sol
@@ -10,49 +10,46 @@ interface ERC223TokenBurner {
 
 contract Burner is ERC223ReceiverMixin {
 
-  event Burn(address burner, uint256 amount, uint8 discount);
+  event Burn(address burner, uint256 amount, uint256 discount);
 
-  uint256 public constant DATE_01_JUNE_2018 = 1527811200;
+  uint256 public constant DATE_01_JUN_2018 = 1527811200;
   uint256 public constant DATE_31_DEC_2018 = 1546214400;
   uint256 public constant DATE_31_DEC_2019 = 1577750400;
   uint256 public constant DATE_31_DEC_2020 = 1609372800;
   uint256 public constant DATE_31_DEC_2021 = 1640908800;
   uint256 public constant DATE_31_DEC_2022 = 1672444800;
 
-  uint256[] public dates = [
-    DATE_31_DEC_2018, 
-    DATE_31_DEC_2019, 
-    DATE_31_DEC_2020, 
-    DATE_31_DEC_2021, 
-    DATE_31_DEC_2022
-  ];
+  struct Phase {
+    uint256 startDate;
+    uint256 endDate;
+    uint256 discount;
+  }
 
-  uint8[] public discounts = [
-    50, 
-    75, 
-    80, 
-    90, 
-    95
-  ];
-
-
+  Phase[] phases;
   ERC223TokenBurner public token;
 
-  constructor(address _tokenBurnerAddress) {
+  constructor(address _tokenBurnerAddress) public {
     token = ERC223TokenBurner(_tokenBurnerAddress);
+    phases.push(Phase(DATE_01_JUN_2018, DATE_31_DEC_2018, 50));
+    phases.push(Phase(DATE_31_DEC_2018 + 1 days, DATE_31_DEC_2019, 75));
+    phases.push(Phase(DATE_31_DEC_2019 + 1 days, DATE_31_DEC_2020, 80));
+    phases.push(Phase(DATE_31_DEC_2020 + 1 days, DATE_31_DEC_2021, 90));
+    phases.push(Phase(DATE_31_DEC_2021 + 1 days, DATE_31_DEC_2022, 95));
   }
 
-  function tokenFallback(address _from, uint256 _value, bytes _data) public {
+  function tokenFallback(address _from, uint256 _value, bytes) public {
     require(msg.sender == address(token));
+      
     // solium-disable-next-line security/no-block-members
-    require(now >= DATE_01_JUNE_2018); 
-    uint256 i = 0;
-    // solium-disable-next-line security/no-block-members
-    while (i < dates.length && dates[i] < now) {
-      i++;
+    for (uint256 i = 0; i < phases.length; i++) {
+      // solium-disable-next-line security/no-block-members
+      if (phases[i].startDate <= now && now <= phases[i].endDate) {
+        require(token.burn(_value));
+        emit Burn(_from, _value, phases[i].discount);
+        break;
+      }
     }
-    assert(i < dates.length);
-    require(token.burn(_value));
-    emit Burn(_from, _value, discounts[i]);
+    assert(i < phases.length);
   }
+
 }
