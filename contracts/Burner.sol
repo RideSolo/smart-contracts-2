466b5def7dcc011057fe3f8e54527cb50c16c7d9
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2018-07-28 19:32:40 +0700

    Change Burner revert logic

--- contracts/Burner.sol
+++ contracts/Burner.sol
@@ -39,16 +39,15 @@ contract Burner is ERC223ReceiverMixin {
 
   function tokenFallback(address _from, uint256 _value, bytes) public {
     require(msg.sender == address(token));
-    uint256 i = 0;
-    for (i; i < phases.length; i++) {
+    for (uint256 i = 0; i < phases.length; i++) {
       // solium-disable-next-line security/no-block-members
       if (phases[i].startDate <= now && now <= phases[i].endDate) {
         require(token.burn(_value));
         emit Burn(_from, _value, phases[i].discount);
-        break;
+        return;
       }
-    } // solium-disable-line indentation
-    require(i < phases.length);
+    } 
+   revert();
   }
 
 }
